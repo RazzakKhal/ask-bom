40efc05ea5eba280da1be4340244d60a610961a8
razzak <khalfallah.razzak@gmail.com>
2026-04-22 14:23:38 +0200

    Maj pom xml / ci

---
+++ common/src/test/java/com/bookndrive/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookndrive.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
