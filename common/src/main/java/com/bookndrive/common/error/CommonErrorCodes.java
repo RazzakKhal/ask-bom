b7d3da5883557b8401c7b974d3c8a6af54b358a0
razzak <khalfallah.razzak@gmail.com>
2026-05-08 23:13:18 +0200

    ask-27 : Ajouter la gestion des erreurs dans chaque MS

---
+++ common/src/main/java/com/bookndrive/common/error/CommonErrorCodes.java
@@ -0,0 +1,12 @@
+package com.bookndrive.common.error;
+
+public final class CommonErrorCodes {
+
+    public static final String VALIDATION_ERROR = "VALIDATION_ERROR";
+    public static final String BAD_REQUEST = "BAD_REQUEST";
+    public static final String ACCESS_DENIED = "ACCESS_DENIED";
+    public static final String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+
+    private CommonErrorCodes() {
+    }
+}
